82eb132e488c0237e97d79f7d1f7a67c0d62224b
Angel Yaranga <angel.yaranga@bitel.com.pe>
2023-07-31 09:52:15 -0500

    Initial commit

---
+++ assets/js/calculator.js
@@ -0,0 +1,5 @@
+const calculatorForm = document.getElementById("calculator");
+
+calculatorForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
---
+++ assets/js/swiper.js
@@ -0,0 +1,27 @@
+const swiper = new Swiper('.swiper', {
+    effect: "coverflow",
+    grabCursor: true,
+    // loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
